d8a9550ac00af85a335407f4a5797123d2d9a391
M. ALZEN <mszen579@hotmail.com>
2018-05-26 10:48:01 +0100

    #5 create reacte client side

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+//App.js
+import React, { Component } from 'react';
+import './index.css';
+import Logreg from './components/Logreg';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";//this is for routing
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <Router>
+            <Switch>
+ 
+            <Route render={function(){
+                return (
+                <p> Not Found
+                <br />
+                      <Link className='btn nav-link btn-success' to='/'>
+                      Back to log and Reg
+                      </Link>
+                </p>
+                )
+            }} />
+            </Switch>
+            </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
