0b99c91d4b990f0d81ed260e47fe70c694181869
muslumgezgin <muslum_gezgin>
2018-05-26 14:41:19 +0200

    #9 finished header footer tempalte about us

--- client/src/components/UserPanel/Aboutus.js
+++ client/src/components/UserPanel/Aboutus.js
@@ -7,7 +7,15 @@ class Aboutus extends Component {
   render() {
     return (
       <div className="App">
-         about us
+        <h1> Stichting Hospice Amsterdam Zuidoost aims to: </h1>
+Organizing hospice care through volunteer work and with the help of the home care organization. This care is offered in Hospice Amsterdam Zuidoost.
+Promoting cooperation with professional assistance such as general practitioners, care institutions and home care organizations.
+The guidance and care of people in the final phase of life.
+The guidance of the relatives of people in their last phase of life. All other activities that aim to shape hospice care.
+You can read the policy plan 2017 - 2020 of Stichting Hospice Amsterdam Zuidoost here.
+What do we offer?
+
+Hospice Amsterdam Zuidoost wants to offer support to people in their last phase of life and to their loved ones. Sometimes it is not possible to spend this last phase at home. That is why Hospice Amsterdam Zuidoost has an 'almost-home' house with 6 places (in the beginning we start with 3 places) for care in the final phase of life for adults aged 18 and over for a period of 3 months. Hospice Amsterdam Zuidoost offers a small-scale facility with a warm and homely feel where people can be themselves and together with their loved ones can freely fulfill their wishes and needs. In addition, Hospice offers Amsterdam Zuidoost a temporary stay for people in respite care. This stay will be temporary because stay in the home situation is not possible at that time.
       </div>
     );
   }
--- client/src/components/UserPanel/Donation.js
+++ client/src/components/UserPanel/Donation.js
@@ -7,7 +7,15 @@ class Donation extends Component {
   render() {
     return (
       <div className="Donation">
-         Donation
+        <h1> Fundraising </h1>
+
+
+
+After looking for funds for some time to support this initiative, we can proudly announce that we have received the following commitments from various funds. 
+
+First of all, we would like to thank all the funds that have so far made a commitment to the hospice 's realization .  The following funds have made a commitment / contribution to the Hospice: Roparun, Stichting Voorzorg Utrecht, Gravin van Bylandt Foundation, National Association of Crematoria and Pasman Foundation.  
+
+With your contribution we can further realize the hospice and soon open our doors. Thank you very much
       </div>
     );
   }
