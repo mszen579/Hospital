934bb72c866f5b68553de0f22854e589a58a8ab5
M. ALZEN <mszen579@hotmail.com>
2018-05-26 14:45:17 +0100

    #10 fixing git

--- client/src/components/UserPanel/Volunteer.js
+++ client/src/components/UserPanel/Volunteer.js
@@ -7,7 +7,20 @@ class Volunteer extends Component {
   render() {
     return (
       <div className="Volunteer">
-         Volunteer
+         <h1>Volunteers wanted </h1>
+
+Hospice Amsterdam Zuidoost will, after opening, mainly depend on volunteers. In addition to the team of coordinators who run the daily routine in the hospice, and the professional caregivers, many activities will be carried out by volunteers.
+
+The volunteers for the Hospice Amsterdam Zuidoost will take care of the residents' homes, offer help with light physical care, offer emotional support to residents or relatives and assist informal caregivers. This work will consist of services of max. 4 consecutive hours, between 7 o'clock in the morning and 11 o'clock in the evening.
+
+The work in the hospice can be heavy for the volunteers, because volunteer work takes place in the sphere of death and death. In addition, the work can also be enriching, because the volunteer contributes to a nice ending to the life of a resident of the hospice. During the voluntary work, the volunteer comes into contact with different people and knowledge is gathered from different cultures.
+
+
+
+Hospice Amsterdam Zuidoost is therefore looking for a group of 75 - 100 volunteers who want to offer support after opening the doors of the hospice. Volunteers are insured by the hospice during their work, receive training with regard to the work, any expenses are reimbursed, care is taken for the mental and physical well-being, and follow-up courses are offered to volunteers who function properly for a considerable period of time.
+
+
+If you are interested, fill in the form below (by pressing the button) and  mail it to: info@hospiceamsterdamzuidoost.nl . For more information please call (06) 442 70 584.
       </div>
     );
   }
